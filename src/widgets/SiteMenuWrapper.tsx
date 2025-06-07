9fa74ce49f695adc2aa99819c0418e24af624229
Артем Антонянц <aaaartemaa@gmail.com>
2025-06-07 21:49:17 +0700

    chore: popover and disable for 0 elements in basket

--- src/widgets/SiteMenuWrapper.tsx
+++ src/widgets/SiteMenuWrapper.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useRouter, usePathname } from "next/navigation";
-import { Button, Menu, MenuProps } from "antd";
+import { Badge, Button, Menu, MenuProps, Popover } from "antd";
 import styles from "./style.module.scss";
 import { useState, useEffect } from "react";
 import { Basket } from "@/features/Basket";
+import { RootState } from "@/shared/store";
+import { useSelector } from "react-redux";
 
 enum Routes {
   products = "/",
@@ -44,15 +46,26 @@ export const SiteMenuWrapper = ({ children }: { children: React.ReactNode }) => 
     setIsBasketOpen(false);
   };
 
+  const basketItems = useSelector((state: RootState) => state.basket.items);
+
+  const totalCount = basketItems.reduce((sum, item) => sum + item.count, 0);
+
   return (
     <>
       <Basket open={isBasketOpen} onCancel={handleBasketCancelClick} />
+
       <div className={styles.container}>
         <div>{children}</div>
       </div>
       <div className={styles.menuContainer}>
         <Menu className={styles.menu} items={items} onClick={handleClick} selectedKeys={[selectedKey]} />
-        <Button onClick={handleButtonClick}>корзина</Button>
+        <Badge count={totalCount}>
+          <Popover content={totalCount ? null : "корзина пуста"}>
+            <Button disabled={totalCount === 0} onClick={handleButtonClick}>
+              корзина
+            </Button>
+          </Popover>
+        </Badge>
       </div>
     </>
   );
