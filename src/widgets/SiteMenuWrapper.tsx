b8e479c8c669661c9408518108f30040c6793fcc
Артем Антонянц <aaaartemaa@gmail.com>
2025-06-07 19:41:04 +0700

    chore: show current open page in menu

--- src/widgets/SiteMenuWrapper.tsx
+++ src/widgets/SiteMenuWrapper.tsx
@@ -1,23 +1,32 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { Menu, MenuProps } from "antd";
 import styles from "./style.module.scss";
+import { useState, useEffect } from "react";
+
+enum Routes {
+  products = "/",
+  reviews = "/reviews",
+}
 
 export const SiteMenuWrapper = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
+  const pathname = usePathname();
+
+  const [selectedKey, setSelectedKey] = useState(pathname);
+
+  useEffect(() => {
+    setSelectedKey(pathname);
+  }, [pathname]);
 
   const items: MenuProps["items"] = [
     {
-      key: "/",
+      key: Routes.products,
       label: "продукты",
     },
     {
-      key: "/basket",
-      label: "корзина",
-    },
-    {
-      key: "/reviews",
+      key: Routes.reviews,
       label: "отзывы",
     },
   ];
@@ -29,7 +38,7 @@ export const SiteMenuWrapper = ({ children }: { children: React.ReactNode }) => 
   return (
     <div className={styles.container}>
       <div>{children}</div>
-      <Menu className={styles.menu} items={items} onClick={handleClick} />
+      <Menu className={styles.menu} items={items} onClick={handleClick} selectedKeys={[selectedKey]} />
     </div>
   );
 };
